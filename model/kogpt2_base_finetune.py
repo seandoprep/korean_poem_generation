7c16effd5a941d89109450e1e3d1823b63a2b926
seandoprep <102036011+seandoprep@users.noreply.github.com>
2023-05-26 22:45:27 +0900

    Update kogpt2_base_finetune.py

--- model/kogpt2_base_finetune.py
+++ model/kogpt2_base_finetune.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # data_arg
     parser.add_argument("--output_dir", type=str, default="./output/kogpt2")
     parser.add_argument(
-        "--data_dir", type=str, default="../data/poem_data/"
+        "--data_dir", type=str, default="./data/poem_data/"
     )
     parser.add_argument("--model_name_or_path", type=str, default="skt/kogpt2-base-v2")
     parser.add_argument("--train_filename", type=str, default="poem_crawled_data.xlsx")
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     parser.add_argument("--seq_len", type=int, default=256)
 
     # huggingface_arg
-    parser.add_argument("REPO_NAME", help='huggingface repository name')
-    parser.add_argument("AUTH_TOKEN", help='huggingface auth token')
+    parser.add_argument("--repo_name", type=str, help='huggingface repository name')
+    parser.add_argument("--auth_token", type=str, help='huggingface auth token')
 
     args = parser.parse_args()
     train(args)

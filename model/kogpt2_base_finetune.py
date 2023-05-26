ba8b1ea5736b113fb14f1fa0f175c924c73e20e1
seandoprep <sean3819@yonsei.ac.kr>
2023-05-26 21:19:57 +0900

    kogpt2

---
+++ model/kogpt2_base_finetune.py
@@ -0,0 +1,27 @@
+import argparse
+
+from kogpt2_finetune import train
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # data_arg
+    parser.add_argument("--output_dir", type=str, default="./output/kogpt2")
+    parser.add_argument(
+        "--data_dir", type=str, default="../data/poem_data/"
+    )
+    parser.add_argument("--model_name_or_path", type=str, default="skt/kogpt2-base-v2")
+    parser.add_argument("--train_filename", type=str, default="poem_crawled_data.xlsx")
+    parser.add_argument("--data_split_ratio", type=float, default=0.9, help="train data ratio")
+
+    # train_arg
+    parser.add_argument("--num_train_epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--seq_len", type=int, default=256)
+
+    # huggingface_arg
+    parser.add_argument("REPO_NAME", help='huggingface repository name')
+    parser.add_argument("AUTH_TOKEN", help='huggingface auth token')
+
+    args = parser.parse_args()
+    train(args)
---
+++ model/kogpt2_finetune.py
@@ -0,0 +1,81 @@
+import torch
+import transformers
+from transformers import AutoModelWithLMHead, PreTrainedTokenizerFast
+from fastai.text.all import *
+import re
+import fastai
+import pandas as pd
+
+#model input output tokenizer
+class TransformersTokenizer(Transform):
+    def __init__(self, tokenizer): self.tokenizer = tokenizer
+    def encodes(self, x):
+        toks = self.tokenizer.tokenize(x)
+        return tensor(self.tokenizer.convert_tokens_to_ids(toks))
+    def decodes(self, x): return TitledStr(self.tokenizer.decode(x.cpu().numpy()))
+
+# gpt2 ouput is tuple, we need just one val
+class DropOutput(Callback):
+    def after_pred(self): self.learn.pred = self.pred[0]
+
+def train(args):
+
+    #download model and tokenizer
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(
+        args.model_name_or_path,
+        bos_token='</s>',
+        eos_token='</s>',
+        unk_token='<unk>',
+        pad_token='<pad>',
+        mask_token='<mask>')
+    model = AutoModelWithLMHead.from_pretrained(args.model_name_or_path)
+
+    added_token_num = tokenizer.add_tokens('/n', '\n')
+    model.resize_token_embeddings(tokenizer.vocab_size + added_token_num)
+
+    # Prepare Huggingface
+    REPO_NAME = args.REPO_NAME  # REPO_NAME(저장할 파일 명) 구분할 수 있게 수정해주기
+    AUTH_TOKEN = args.AUTH_TOKEN
+
+    # Prepare data
+    data_path = os.path.join(args.data_dir, args.train_filename)
+    data = pd.read_excel(data_path)
+    data_list = list(data['content'])
+
+    with open('poemlove.txt', 'w', encoding='UTF-8') as f:
+        for poem in data_list:
+          f.write(poem + '\n')
+
+    with open('poemlove.txt','r', -1, "utf-8") as f:
+        lines = f.read()
+
+    lines = " ".join(lines.split())
+
+    #split data
+    train = lines[:int(len(lines)*args.data_split_ratio)]
+    test = lines[int(len(lines)*args.data_split_ratio): int(len(lines))]
+    splits = [[0],[1]]
+
+    #init dataloader
+    tls = TfmdLists([train,test], TransformersTokenizer(tokenizer), splits=splits, dl_type=LMDataLoader)
+    batch,seq_len = args.batch_size, args.seq_len
+    dls = tls.dataloaders(bs=batch, seq_len=seq_len)
+
+    learn = Learner(dls, model, loss_func=CrossEntropyLossFlat(), cbs=[DropOutput], metrics=Perplexity()).to_fp16()
+    lr = learn.lr_find()
+    learn.fine_tune(args.num_train_epochs)
+
+    model.push_to_hub(REPO_NAME,
+                      use_temp_dir=True,
+                      use_auth_token=AUTH_TOKEN
+                      )
+    tokenizer.push_to_hub(REPO_NAME,
+                      use_temp_dir=True,
+                      use_auth_token=AUTH_TOKEN
+                      )
+
+    model.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
+
+
+
